c960d6c88ff846c845a216e571b69ba45d8bcbd1
Kassandra Castro <kmc@KassandrasMBP2.attlocal.net>
2019-08-29 14:34:14 -0400

    initial site, no design

---
+++ scripts.js
@@ -0,0 +1,14 @@
+﻿
+function show(category){
+    document.querySelectorAll(".detail").forEach(function(detail){
+        detail.style.display = "none";
+    });
+    console.log(category);
+    document.getElementById(category).style.display = "block";
+
+    if (document.getElementById("nav").style.display == "none") {
+        document.getElementById("nav").style.display = "flex";
+    }
+
+    window.scrollTo(0, 0);
+}
